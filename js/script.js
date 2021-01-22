4a7f934117ca688f36e63a66fa1d578777f30de6
Andrew Maiorov <Maiorov23@yandex.ru>
2021-01-22 21:18:01 +0300

    add header

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});;
+
+let burgerMenu = document.querySelector('.header__burger');
+let iteratingClass = document.querySelectorAll('.header__burger,.header__menu')
+let body = document.body;
+burgerMenu.addEventListener('click', (e) => {
+	iteratingClass.forEach(item => item.classList.toggle("active"))
+	body.classList.toggle('lock');
+});
